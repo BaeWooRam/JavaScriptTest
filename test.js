981d8aa6bd711ee044e35d5d7eba124ab4274f0f
BaeWooRam <41356481+BaeWooRam@users.noreply.github.com>
2021-05-28 10:54:26 +0900

    Initial commit

---
+++ test.js
@@ -0,0 +1,12 @@
+var book = {
+    topic : "JavaScript",
+    fat : true
+}
+
+var point = [
+    {x:0, y:1},
+    {x:3, y:2}
+]
+
+console.log("book fat = "+book.fat+", topic = "+book.topic)
+console.log("point[0] x = "+point[0].x+", y = "+point[0].y+", point[1] x = "+point[1].x+", y = "+point[1].y)
